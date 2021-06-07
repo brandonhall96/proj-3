c37278c3055b0dc2dfb1a704317ccfd996bb8b9b
brandonhall96 <hall.bran96@gmail.com>
2021-06-07 18:27:47 -0400

    installed dependencies, added component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
