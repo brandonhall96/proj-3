c916661509d8006b5ad7141a82d8579ad9528a6a
brandonhall96 <hall.bran96@gmail.com>
2021-06-07 18:31:26 -0400

    creating imports in app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import './App.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import Dashboard from './components/Dashboard';
+import Homepage from './components/Homepage';
+import Rockets from './components/Rockets';
+import StarLink from './components/StarLink';
+import Tracker from './components/Tracker';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
